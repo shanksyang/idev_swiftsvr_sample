6d7ec37e2bb83ecc6dabb4a44c543c2a1df726dd
shanks <shanks@wisdomtmt.com>
2016-10-31 22:46:12 +0800

    add post

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,8 +2,33 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get("/idev_restful") { _ in
-    return "Hello idev"
+drop.get("/idev_restful") { request in
+    guard let userId = request.data["id"]?.string else {
+        throw Abort.badRequest
+    }
+    return try JSON(node: [
+        "id": userId,
+        "name": "shanks"
+        ])
+    
 }
 
+drop.post("/idev_restful") { request in
+    guard let userId = request.data["id"]?.string, let userName = request.data["name"]?.string else {
+        throw Abort.badRequest
+    }
+    return try JSON(node: [
+        "id": userId,
+        "name": userName
+        ])
+
+}
+
+drop.put("/idev_restful") { request in
+        return "Hello idev put"
+}
+
+drop.delete("/idev_restful") { request in
+    return "Hello idev delete"
+}
 drop.run()
